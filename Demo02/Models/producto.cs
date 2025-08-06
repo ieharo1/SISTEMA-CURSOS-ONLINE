61eebb9a47a257b10091ecf72635bc18f597c678
ieharo1 <zackharo1@gmail.com>
2025-08-06 12:06:10 -0500

    Primer commit

---
+++ Demo02/Models/producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo02.Models
+{
+    public class producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public decimal pvp { get; set; }
+        public int stock { get; set; }
+        public string categoria { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime fecha_elaboracion { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime fecha_caducidad { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ Demo02/Pages/Productos/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Demo02.Data;
+using Demo02.Models;
+
+namespace Demo02.Pages.Productos
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Demo02.Data.Demo02Context _context;
+
+        public DeleteModel(Demo02.Data.Demo02Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public producto producto { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            producto = await _context.producto.FirstOrDefaultAsync(m => m.id == id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            producto = await _context.producto.FindAsync(id);
+
+            if (producto != null)
+            {
+                _context.producto.Remove(producto);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
